1a969ad42392b829610e60aa0423edc1d9fffe1d
Vasco Jesus <jesusvasco0@gmail.com>
2025-03-07 20:41:34 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import tweepy
+import os
+import random
+from dotenv import load_dotenv
+
+load_dotenv()
+
+consumer_key = os.getenv("CONSUMER_KEY")
+consumer_secret = os.getenv("CONSUMER_SECRET")
+access_token = os.getenv("ACCESS_TOKEN")
+access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+bearer_token = os.getenv("BEARER_TOKEN") 
+
+
+client = tweepy.Client(
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token=access_token,
+    access_token_secret=access_token_secret
+)
+
+auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
+api = tweepy.API(auth)
+
+images_dir = "images"
+
+date_folders = [os.path.join(images_dir, d) for d in os.listdir(images_dir) if os.path.isdir(os.path.join(images_dir, d))]
+
+random_folder = random.choice(date_folders)
+
+image_files = [os.path.join(random_folder, f) for f in os.listdir(random_folder) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+
+random_image = random.choice(image_files)
+
+counter_file = "tweet_counter.txt"
+
+if not os.path.exists(counter_file):
+    day_count = 1
+else:
+    with open(counter_file, "r") as f:
+        day_count = int(f.read().strip()) + 1
+
+with open(counter_file, "w") as f:
+    f.write(str(day_count))
+
+media = api.media_upload(filename=random_image)
+
+tweet_text = f"Day {day_count}"
+response = client.create_tweet(text=tweet_text, media_ids=[media.media_id_string], user_auth=True)
+
+print("Tweet posted successfully!", response)
